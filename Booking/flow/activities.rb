b8e8ccfee351679e221d971a9734a18c2e967639
Shou Takenaka <sasaminn@gmail.com>
2017-09-08 13:32:40 +0900

    Add booking worker, activity and starter

--- Booking/flow/activities.rb
+++ Booking/flow/activities.rb
@@ -1,1 +1,28 @@
-# You can require your activity files here.
+# BookingActivity class defines a set of activities for the Booking sample.
+class BookingActivity
+  extend AWS::Flow::Activities
+
+  # The activity method is used to define activities. It accepts a list of names
+  # of activities and a block specifying registration options for those
+  # activities
+  activity :reserve_car, :reserve_air, :send_confirmation do
+    {
+      version: "1.0",
+    }
+  end
+
+  # This activity can be used to reserve a car for a given request_id
+  def reserve_car(request_id)
+    puts "Reserving car for Request ID: #{request_id}\n"
+  end
+
+  # This activity can be used to reserve a flight for a given request_id
+  def reserve_air(request_id)
+    puts "Reserving airline for Request ID: #{request_id}\n"
+  end
+
+  # This activity can be used to send a booking confirmation to the customer
+  def send_confirmation(customer_id)
+    puts "Sending notification to customer: #{customer_id}\n"
+  end
+end#
--- Booking/flow/workflows.rb
+++ Booking/flow/workflows.rb
@@ -1,1 +1,55 @@
-# You can require your workflow files here.
+require 'flow/activities'
+
+# BookingWorkflow class defines the workflows for the Booking sample
+class BookingWorkflow
+  extend AWS::Flow::Workflows
+
+  # Use the workflow method to define workflow entry point.
+  workflow :make_booking do
+    {
+      version: "1.0",
+      default_execution_start_to_close_timeout: 120
+    }
+  end
+
+  # Create an activity client using the activity_client method to schedule
+  # activities
+  activity_client(:client) { { from_class: "BookingActivity" } }
+
+  # This is the entry point for the workflow
+  def make_booking options
+
+    puts "Workflow has started\n" unless is_replaying?
+    # This array will hold all futures that are created when asynchronous
+    # activities are scheduled
+    futures = []
+
+    if options[:reserve_car]
+      puts "Reserving a car for customer\n" unless is_replaying?
+      # The activity client can be used to schedule activities
+      # asynchronously by using the send_async method
+      futures << client.send_async(:reserve_car, options[:request_id])
+    end
+    if options[:reserve_air]
+      puts "Reserving air ticket\n" unless is_replaying?
+      futures << client.send_async(:reserve_air, options[:customer_id])
+    end
+
+    puts "Waiting for reservation to complete\n" unless is_replaying?
+    # wait_for_all is a flow construct that will wait on the array of
+    # futures passed to it
+    wait_for_all(futures)
+
+    # After waiting on the reservation activities to complete, the workflow
+    # will call the send_confirmation activity.
+    client.send_confirmation(options[:customer_id])
+
+    puts "Workflow has completed\n" unless is_replaying?
+  end
+
+  # Helper method to check if Flow is replaying the workflow. This is used to
+  # avoid duplicate log messages
+  def is_replaying?
+    decision_context.workflow_clock.replaying
+  end
+end
